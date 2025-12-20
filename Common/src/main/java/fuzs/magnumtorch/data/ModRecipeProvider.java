2e5c43e9b11dc3ec74eef353d8d91dbdeabb1140
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-12-20 23:39:11 +0100

    prepare 1.21.11 port

---
+++ Common/src/main/java/fuzs/magnumtorch/data/ModRecipeProvider.java
@@ -0,0 +1,38 @@
+package fuzs.magnumtorch.data;
+
+import fuzs.magnumtorch.init.ModRegistry;
+import fuzs.puzzleslib.api.data.v2.AbstractRecipeProvider;
+import fuzs.puzzleslib.api.data.v2.core.DataProviderContext;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
+
+public class ModRecipeProvider extends AbstractRecipeProvider {
+
+    public ModRecipeProvider(DataProviderContext context) {
+        super(context);
+    }
+
+    @Override
+    public void addRecipes(RecipeOutput recipeOutput) {
+        this.magnumTorch(recipeOutput, ModRegistry.DIAMOND_MAGNUM_TORCH_BLOCK.value(), Items.DIAMOND);
+        this.magnumTorch(recipeOutput, ModRegistry.EMERALD_MAGNUM_TORCH_BLOCK.value(), Items.EMERALD);
+        this.magnumTorch(recipeOutput, ModRegistry.AMETHYST_MAGNUM_TORCH_BLOCK.value(), Items.AMETHYST_SHARD);
+    }
+
+    public final void magnumTorch(RecipeOutput recipeOutput, ItemLike resultItem, ItemLike ingredientItem) {
+        ShapedRecipeBuilder.shaped(this.items(), RecipeCategory.DECORATIONS, resultItem)
+                .define('L', ItemTags.LOGS)
+                .define('T', Items.FIRE_CHARGE)
+                .define('G', Items.GOLD_INGOT)
+                .define('#', ingredientItem)
+                .pattern("GTG")
+                .pattern("#L#")
+                .pattern("#L#")
+                .unlockedBy(getHasName(ingredientItem), this.has(ingredientItem))
+                .save(recipeOutput);
+    }
+}
